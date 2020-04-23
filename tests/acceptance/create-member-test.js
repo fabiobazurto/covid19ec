a7db4a23e634ef6869f413f0f4ac19facc484714
Fabio A. Bazurto <fabio.bazurto@gmail.com>
2020-04-22 19:59:42 -0500

    testing existing member

--- tests/acceptance/create-member-test.js
+++ tests/acceptance/create-member-test.js
@@ -6,6 +6,7 @@ import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 
 import createProvinces from '../helpers/create-provinces';
+import createMembers from '../helpers/create-members';
 
 module('Acceptance | create member', function(hooks) {
 
@@ -26,6 +27,7 @@ module('Acceptance | create member', function(hooks) {
         assert.equal(currentURL(), '/', 'user authenticated');
 	
         let provinces = createProvinces(10);
+	
         this.set('model',provinces);
         
 	await visit('/members/new');
@@ -59,5 +61,73 @@ module('Acceptance | create member', function(hooks) {
         await click('button.goFinish');        
         
         assert.equal(currentURL(), '/members/new/confirmation','Finalizando');
+
+	await click('.btn-finish');
+
+        assert.equal(currentURL(), '/members','Cargando miembros');
+
+	const contactrows = this.element.querySelectorAll('.member-row');
+
+        assert.equal(contactrows.length, 1, 'Contando miembros');
+	
     });
+    
+    test('creating existing member', async function(assert) {
+        await authenticateSession({
+            userId: 1,
+            accessToken: 'abcdefg123456',
+            uuid: '123456',
+            client: 'fakeclient',
+            expiry: '9999999999',
+            fullname: 'Faker Faker'
+	});
+	await visit('/');
+
+        assert.equal(currentURL(), '/', 'user authenticated');
+
+
+	let existingmember = createMembers(1)
+	
+        let provinces = createProvinces(10);
+        this.set('model',provinces);
+        
+	await visit('/members/new');
+        
+        await fillIn('input[name="firstName"]', 'XFabio');
+        await fillIn('input[name="lastName"]', 'XBazurto');
+        await fillIn('input[name="nationalId"]', '0921679346');
+
+        await click('button.btn-primary');
+
+        assert.equal(currentURL(), '/members/new/demographics', 'demographics OK');
+        
+        await click('input[name="gender"]:first-child');
+        await fillIn('select[name="cbo_provinces"]',"2");
+        await fillIn('input[name="city"]', 'GUAYAQUIL');        
+       
+        await click('button.gonext');
+
+        assert.equal(currentURL(), '/members/new/isAlive','isAlive OK');        
+
+        await click('input[name="alive"]:first-child');
+        
+        await click('button.gonext');
+
+        assert.equal(currentURL(), '/members/new/wasPicked','Preguntas OK');
+
+        await click('.sintom');
+
+        await click('button.goFinish');        
+        
+        assert.equal(currentURL(), '/members/new/confirmation','Finalizando');
+
+	await click('.btn-finish');
+
+        assert.equal(currentURL(), '/members','Cargando miembros');
+
+
+
+
+	
+    });    
 });
---
+++ tests/helpers/create-members.js
@@ -0,0 +1,25 @@
+import { computed } from '@ember/object';
+import EmberObject from '@ember/object';
+
+export default function createMembers(records){
+    const contactObjects=[];
+    
+    for(let i=0;i<records;i++){
+        const contactObject = EmberObject.extend({
+            name: computed(()=> 'Guayaquil'),
+	    firstName: computed(()=> 'Fabio'),
+	    lastName:  computed(()=> 'Bazurto'),
+	    nationalId: computed(()=> '0921679346'),
+	    status: computed(()=> '0'),
+	    gender: computed(()=> 'M'),
+	    province: computed(()=> '1'),
+	    city: computed(()=> 'Guayaquil'),
+	    picked: computed(()=> true),
+	    alive: computed(()=> false),
+	    
+        }).create();
+
+        contactObjects.push(contactObject);
+    }
+    return contactObjects;
+}
